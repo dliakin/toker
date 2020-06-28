ac61c23cb7b9e74a27fa5c9560b174a884ce7a0a
Dmitry Lyakin <d.lyakin@gmail.com>
2020-06-28 15:44:53 +0300

    Добавил проверку в обработчик ошибок на несуществующий атрибут

--- client/src/redux/sagas/safeSaga.js
+++ client/src/redux/sagas/safeSaga.js
@@ -7,13 +7,13 @@ export function safeSaga(func) {
             yield* func(args);
         } catch (error) {
             console.log(error)
-            if (error.response.status && error.response.status === 402) {
+            if (error.response && error.response.status && error.response.status === 402) {
                 yield put({ type: NEED_PAY })
             }
-            else if (error.response.status && error.response.status === 401) {
+            else if (error.response && error.response.status && error.response.status === 401) {
                 yield put({ type: LOGOUT })
             }
-            else if (error.response.data.message) {
+            else if (error.response && error.response.data.message) {
                 yield put({ type: SHOW_ALERT, payload: { msg: error.response.data.message, type: 'error' } })
             } else {
                 yield put({ type: SHOW_ALERT, payload: { msg: 'Что-то пошло не так. Попробуйте ещё раз', type: 'error' } })
