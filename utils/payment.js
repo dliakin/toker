af81e391e7b2626c463a6c839efaeba99bb128c1
Dmitry Lyakin <d.lyakin@gmail.com>
2020-07-07 21:25:58 +0300

    FIX Купоны

--- utils/payment.js
+++ utils/payment.js
@@ -2,7 +2,7 @@ const config = require('config')
 const r2 = require('r2')
 const models = require('../models')
 
-const createPay = async (plan_id, user_id) => {
+const createPay = async (plan_id, user_id, coupon) => {
 
     const prefix = `toker${process.env.NODE_ENV}`
 
@@ -22,6 +22,14 @@ const createPay = async (plan_id, user_id) => {
         userId: user_id,
         planId: plan.id
     })
+
+    if (coupon === "lastchance") {
+        plan.price = plan.price - 100 * plan.duration
+    }
+
+    if (coupon === "SJDHjiNXUIXNnndU") {
+        plan.price = plan.price - 989 * plan.duration
+    }
 
     const url = `https://shipe.ru/chat/pay_travel.php?key=${config.get("tinkoffKey")}&func=gUP&mail=${user.email}&id=${prefix}_${pay.id}_${plan.id}&summ=${plan.price * 100}&lang=ru`
 
