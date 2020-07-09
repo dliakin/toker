10f2bc77825dfa039f9b50331884704993d7ddff
Dmitry Lyakin <d.lyakin@gmail.com>
2020-07-09 14:01:31 +0300

    Fix TildaUsers

---
+++ migrations/20200709103931-add_paidto_column_to_tildauser.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await queryInterface.addColumn(
+        'TildaUsers',
+        'paidTo',
+        { type: Sequelize.DATE },
+        { transaction },
+      )
+      await transaction.commit();
+      return Promise.resolve();
+    } catch (err) {
+      if (transaction) {
+        await transaction.rollback();
+      }
+      return Promise.reject(err);
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await queryInterface.removeColumn('TildaUsers', 'paidTo')
+      await transaction.commit();
+      return Promise.resolve();
+    } catch (err) {
+      if (transaction) {
+        await transaction.rollback();
+      }
+      return Promise.reject(err);
+    }
+  }
+};
--- models/tildauser.js
+++ models/tildauser.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const TildaUser = sequelize.define('TildaUser', {
-    email: DataTypes.STRING
+    email: DataTypes.STRING,
+    paidTo: DataTypes.DATE,
   }, {});
   TildaUser.associate = function(models) {
     // associations can be defined here
